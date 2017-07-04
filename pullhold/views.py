216e434302d96ee171e43606aca477a6ff917d2a
Alex Orear <alex.orear@gmail.com>
2017-07-04 11:30:58 -0500

    Removed unused imports from views.py

--- pullhold/views.py
+++ pullhold/views.py
@@ -1,12 +1,7 @@
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from django.core.urlresolvers import reverse_lazy
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
-from django.views import generic
 from django.views.generic import View
-
 from .forms import UserForm
-from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
